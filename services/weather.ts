064c46aee7aa414dc7e99c063ee83da27a7b5dcd
Rafaela-Quinzel <rafaelaquinzel@gmail.com>
2026-01-07 12:22:10 -0300

    feat: add weather api types

--- services/weather.ts
+++ services/weather.ts
@@ -1,6 +1,10 @@
+import { WeatherResponse } from '@/types/weather';
+
 const API_URL = 'https://api.openweathermap.org/data/2.5';
 
-export async function getCurrentWeather(city: string) {
+export async function getCurrentWeather(
+  city: string
+): Promise<WeatherResponse> {
   const response = await fetch(
     `${API_URL}/weather?q=${city}&units=metric&lang=pt_br&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
   );
