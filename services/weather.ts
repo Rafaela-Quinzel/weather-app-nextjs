0f8fc559c0374b03ee87b14ecfb64a8c483ac28f
Rafaela-Quinzel <rafaelaquinzel@gmail.com>
2026-01-07 12:16:27 -0300

    feat: add weather api service

---
+++ services/weather.ts
@@ -0,0 +1,13 @@
+const API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export async function getCurrentWeather(city: string) {
+  const response = await fetch(
+    `${API_URL}/weather?q=${city}&units=metric&lang=pt_br&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error('Cidade não encontrada');
+  }
+
+  return response.json();
+}
